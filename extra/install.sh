900d9dd1b22cc20651b23f1c433d353edfd2d2fe
Matthew Pietz <git@matthewpietz.me>
2012-01-17 23:10:33 -0800

    quote strings on install.sh

--- extra/install.sh
+++ extra/install.sh
@@ -5,20 +5,20 @@ NOW=$(date +%s)
 
 cd "$HOME"
 if [ -f .vimrc -a ! -h .vimrc ]; then
-	echo Backing up old vimrc...
+	echo "Backing up old vimrc..."
 	mv .vimrc .vimrc-saved-$NOW
 fi
 
-echo Linking new vimrc...
+echo "Linking new vimrc..."
 ln -sf .vim/vimrc.vim .vimrc
 
 cd "$RCDIR"
 
-echo Initializing submodules...
+echo "Initializing submodules..."
 git submodule update --init
 
 if [ ! -f vimrc.local.vim ]; then
-	echo Copying sample local vimrc...
+	echo "Copying sample local vimrc..."
 	cp extra/vimrc.local.sample vimrc.local.vim
-	echo You can now open and customize ~/.vim/vimrc.local.vim
+	echo "You can now open and customize ~/.vim/vimrc.local.vim"
 fi
